a48c40b5688ae7a42a93def76d55b6bd67583970
fengguowudi <18614071206@163.com>
2023-04-10 20:38:23 +0800

    Update crawl.go

--- crawler/crawl.go
+++ crawler/crawl.go
@@ -2,12 +2,35 @@ package crawler
 
 import (
 	"errors"
+	"net"
 	"net/http"
 	"net/url"
 	"runtime"
+	"time"
+
+	"github.com/bits-and-blooms/bloom/v3"
 )
 
-var client = http.Client{}
+var (
+	client *http.Client
+	filter *bloom.BloomFilter
+)
+
+func init() {
+	transport := &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout: 10 * time.Second,
+		}).DialContext,
+		MaxIdleConns:        10,
+		IdleConnTimeout:     30 * time.Second,
+		MaxIdleConnsPerHost: 5,
+	}
+	client = &http.Client{
+		Timeout:   30 * time.Second,
+		Transport: transport,
+	}
+	filter = bloom.NewWithEstimates(1000000, 0.01)
+}
 
 func genUA() string {
 	switch runtime.GOOS {
@@ -27,6 +50,11 @@ func Crawl(target, referer string) (*http.Response, error) {
 	if _, err := url.Parse(target); err != nil {
 		return nil, err
 	}
+
+	if filter.TestString(target) {
+		return nil, errors.New("duplicate page")
+	}
+
 	req, err := http.NewRequest(http.MethodGet, target, nil)
 	if err != nil {
 		return nil, err
@@ -37,13 +65,18 @@ func Crawl(target, referer string) (*http.Response, error) {
 	req.Header.Set("Accept-Encoding", "gzip")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9")
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return nil, errors.New("bad status code: " + resp.Status)
 	}
+
+	filter.AddString(target)
 
 	return resp, nil
 }
